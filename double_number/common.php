c51eb9d7ac8f5ef8ef9dd8378a7ce50d4b76ba3f
Polit <1828890209@qq.com>
2019-01-29 11:08:45 +0800

    更新注释

--- double_number/common.php
+++ double_number/common.php
@@ -1,18 +1,26 @@
 <?php
 // 因为项目里面一个金额支付问题，被截取的位数总是会进1，然后小数点截取方法使用了 substr 和 sprintf("%.9f",……) 这样的方法，和使用floor()先乘再除一样，会丢失精度导致截取位数再往后一位为某个数字（比如9）时，会进一位，遂干脆自己写一个， 用纯字符串截取，毛事都没有了。
-/** 截取浮点数的小数点位数，默认8位 */
+/** 
+  * 截取浮点数的小数点位数，默认8位<br/>
+  * @param double $double_val 浮点数
+  * @param int $point_size 需要截取的小数点后位数
+  * @return double 截取后的浮点数
+   */
 function formatDoubleVal($double_val,$point_size=8){
     $double_val=doubleval($double_val);
     $point_size=intval($point_size);
     if(!$point_size || $point_size<=0 ) $point_size=8;
-    var_dump($point_size);
     if(!strpos($double_val,".")) return $double_val;
     $double_val=explode('.',$double_val);
     $double_val[1]= substr($double_val[1],0,$point_size);
     return doubleval($double_val[0].".".$double_val[1]);
 }
 
-/** 获取小数点后的位数 */ //网络流传方法
+/** 
+  * 获取小数点后的位数；<br/>
+  * @param double $double_val 浮点数
+  * @return int 位数
+  */ //网络流传方法
 function getFloatLength($double_val) {
     $count=0;$num=doubleval($double_val);
     $temp=explode('.',$double_val);
