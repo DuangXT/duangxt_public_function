b518df6746f983fd1eda7be9de4a2ea6c3918658
Polit <1828890209@qq.com>
2019-02-01 14:16:59 +0800

    #

--- time/common.php
+++ time/common.php
@@ -4,6 +4,7 @@
  * 你可以用microtime()获取时间戳和毫秒，也可以用microtime(true)获取float的带毫秒时间戳。<br/>
  * 但是你可能会发现microtime(true)得到的float只有4位。即便将microtime()得到的数拆开相加，也是4位。<br/>
  * 于是写了这个方法。
+ * @return double 带毫秒的float时间戳
  */
 function getMicrotime(){
     $time = explode(' ', microtime());
